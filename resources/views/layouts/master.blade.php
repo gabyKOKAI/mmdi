3ef1c4de6852d4fcab8a508a51e2f64eae8354a8
gabyKOKAI <gaby@kokai.com.mx>
2018-03-23 19:06:48 -0600

    Boostrap Fix

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,6 +7,7 @@
 
     <meta charset='utf-8'>
     <link href="/css/mmdi.css" type='text/css' rel='stylesheet'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <!--script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script-->
